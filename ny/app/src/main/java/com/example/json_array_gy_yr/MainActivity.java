374a8fb97ae2a6d6d5dbbe5bc39961b5a62e0a65
yura970804 <simyura1@gmail.com>
2020-11-07 14:07:17 +0900

    나영

---
+++ ny/app/src/main/java/com/example/json_array_gy_yr/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.json_array_gy_yr;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int sub = 1001;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.sub);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),SubActivity.class);
+                startActivityForResult(intent,sub);
+            }
+        });
+    }
+}
